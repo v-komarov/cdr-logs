d23e239d9aedfbd0765166a1d445d16750394d6c
Vladimir Komarov <komarov-krsk@mail.ru>
2019-04-12 09:22:07 +0700

    screen

---
+++ screen/chi-asterisk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+FLUME_HOME=/opt/flume
+
+/opt/flume/bin/flume-ng agent --conf $FLUME_HOME/conf --conf-file $FLUME_HOME/conf/chi-asterisk.conf --name NetcatAgent -Dflume.root.logger=INFO,console
